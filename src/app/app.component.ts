6cef5f42145d578f95ea75f044e43e0fe3689469
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-23 23:10:59 +0200

    add food categories interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FoodCategories } from './food-categories';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chukwudi';
 }
+
+export const FOODCATEGORIES: FoodCategories[] = [
+  { id: 1, title: 'All', imgPath: '/assets/food.svg ' },
+  { id: 2, title: 'Pizza', imgPath: '/assets/food.svg ' },
+  { id: 3, title: 'Asian', imgPath: '/assets/food.svg ' },
+  { id: 4, title: 'Burgers', imgPath: '/assets/food.svg ' },
+  { id: 5, title: 'Barbecue', imgPath: '/assets/food.svg ' },
+  { id: 6, title: 'Dessers', imgPath: '/assets/food.svg ' },
+  { id: 7, title: 'Thai', imgPath: '/assets/food.svg ' },
+  { id: 8, title: 'Sushi', imgPath: '/assets/food.svg ' },
+];
---
+++ src/app/food-categories.ts
@@ -0,0 +1,5 @@
+export interface FoodCategories {
+  id: number;
+  title: string;
+  imgPath: string;
+}

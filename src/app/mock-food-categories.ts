24e0678b4906a3f2dfe8a95796830a28e275423a
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-24 09:20:16 +0200

    Update food categories icons

--- src/app/mock-food-categories.ts
+++ src/app/mock-food-categories.ts
@@ -2,11 +2,11 @@ import { FoodCategories } from './food-categories';
 
 export const FOODCATEGORIES: FoodCategories[] = [
   { id: 1, title: 'All', imgPath: '/assets/food.svg ' },
-  { id: 2, title: 'Pizza', imgPath: '/assets/food.svg ' },
-  { id: 3, title: 'Asian', imgPath: '/assets/food.svg ' },
-  { id: 4, title: 'Burgers', imgPath: '/assets/food.svg ' },
-  { id: 5, title: 'Barbecue', imgPath: '/assets/food.svg ' },
-  { id: 6, title: 'Dessers', imgPath: '/assets/food.svg ' },
-  { id: 7, title: 'Thai', imgPath: '/assets/food.svg ' },
-  { id: 8, title: 'Sushi', imgPath: '/assets/food.svg ' },
+  { id: 2, title: 'Pizza', imgPath: '/assets/pizza.svg ' },
+  { id: 3, title: 'Asian', imgPath: '/assets/asian.svg ' },
+  { id: 4, title: 'Burgers', imgPath: '/assets/burgers.svg ' },
+  { id: 5, title: 'Barbecue', imgPath: '/assets/barbecue.svg ' },
+  { id: 6, title: 'Dessers', imgPath: '/assets/dessers.svg ' },
+  { id: 7, title: 'Thai', imgPath: '/assets/thai.svg ' },
+  { id: 8, title: 'Sushi', imgPath: '/assets/sushi.svg ' },
 ];

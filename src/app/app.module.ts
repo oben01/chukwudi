2382b005bfc50ad2105df5df4138181cd8838d3f
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-24 11:24:15 +0200

    [+] restaurant card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FoodCategoriesCardComponent } from './food-categories-card/food-categories-card.component';
+import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FoodCategoriesCardComponent
+    FoodCategoriesCardComponent,
+    RestaurantCardComponent
   ],
   imports: [
     BrowserModule,
